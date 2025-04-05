3c762b05890e696afb2f748ea707720986d4b299
Pavlo Slovikovskyi <slovikovskuy06@gmail.com>
2025-04-05 19:06:47 +0300

    feat: created new page with product list
    lab 8 done

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Product List</h1>
+    <ul class="list-disc list-inside">
+      <li>Milk</li>
+      <li>Cookies</li>
+      <li>Eggs</li>
+      <li>Flour</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: 'Список продуктів'
+})
+</script>
